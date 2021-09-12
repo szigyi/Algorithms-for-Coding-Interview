47e317faef5063485c00dc52c49fbca57e839b4a
Sabi <szigyi.szogyi@gmail.com>
2021-09-12 15:06:02 +0100

    add official is unique string exercise

--- ctci/1arrayandstring/src/main/scala/IsUniqueString.scala
+++ ctci/1arrayandstring/src/main/scala/IsUniqueString.scala
@@ -1,7 +1,21 @@
+import scala.collection.mutable
+
 object IsUniqueString {
 
-  def isUnique(s: String): Boolean = {
+  def isUnique(s: String): Boolean =
     if (s.isEmpty) false
     else s.toSet.size == s.length
+
+  def isUniqueOfficial(s: String): Boolean = {
+    if (s.length > 128) false
+    else {
+      val charset = mutable.ArrayBuffer[Boolean]()
+      for { i <- 0 until s.length } yield {
+        val c = s.charAt(i)
+        if (charset(c)) return false
+        else charset(c) = true
+      }
+      true
+    }
   }
 }
