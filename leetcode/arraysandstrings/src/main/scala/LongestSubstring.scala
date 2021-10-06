c10c15d9cedb33e5c8f83034fe23820cfe9849ed
Sabi <szigyi.szogyi@gmail.com>
2021-10-06 19:11:58 +0100

    very ugly solution

--- leetcode/arraysandstrings/src/main/scala/LongestSubstring.scala
+++ leetcode/arraysandstrings/src/main/scala/LongestSubstring.scala
@@ -1,22 +1,81 @@
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 object LongestSubstring {
 
-  def longestSubstring(s: String): Int = {
-    var uniqueChars = mutable.Set[Char]()
-    var maxLength = 0
-    var lengthOfUniqueString = 0
-    s.map { c =>
-      if (uniqueChars.contains(c)) {
-        uniqueChars = mutable.Set[Char]()
-        if (lengthOfUniqueString > maxLength) maxLength = lengthOfUniqueString
-        uniqueChars.add(c)
-        lengthOfUniqueString = 1
+  def longestSubstring(str: String): Int = {
+    def isThereAnotherDuplicateCharWithinRange(i: Int, j: Int, indices: Iterable[Set[Int]]): Boolean =
+      indices.map { ixs =>
+        ixs.count(index => index > i && index < j) > 1
+      }.exists(_ == true)
+
+    def biggestIndexDifference(indices: ListBuffer[Int], indicesCache: Iterable[Set[Int]]): Int = {
+      var maxDistance = 0
+      var prevPreviousIndex = -1
+      var previousIndex = 0
+      indices.map { i =>
+        prevPreviousIndex match {
+          case p if p >= 0 =>
+            val distance = i - prevPreviousIndex - 1
+            if (distance > maxDistance) {
+              val subIndices = isThereAnotherDuplicateCharWithinRange(prevPreviousIndex, i, indicesCache)
+              if (!subIndices) {
+                println(s"subIndices:$subIndices, prevPreviousIndex:${prevPreviousIndex + 1}, previousIndex:$previousIndex, i:$i")
+                maxDistance = distance
+              }
+            }
+          case p =>
+            val distance = i - previousIndex
+            if (distance > maxDistance) {
+              val subIndices = isThereAnotherDuplicateCharWithinRange(previousIndex, i, indicesCache)
+              if (!subIndices) {
+                println(s"subIndices:$subIndices, prevPreviousIndex:${prevPreviousIndex + 1}, previousIndex:$previousIndex, i:$i")
+                maxDistance = distance
+              }
+            }
+        }
+        prevPreviousIndex = previousIndex
+        previousIndex = i
+      }
+      println(s"maxDistance:$maxDistance, indices:$indices")
+      maxDistance
+    }
+
+    val chars = mutable.Map[Char, ListBuffer[Int]]()
+    str.zipWithIndex.map { case (c, i) =>
+      if (chars.contains(c)) {
+        chars(c).addOne(i)
       } else {
-        uniqueChars.add(c)
-        lengthOfUniqueString += 1
+        chars.addOne(c, ListBuffer[Int](i))
       }
     }
-    Math.max(lengthOfUniqueString, maxLength)
+    println(s"chars:$chars")
+    val duplicates = chars.values.filter(_.size > 1)
+    println(s"duplicates:$duplicates")
+    val indicesCache = duplicates.map(_.toSet)
+    if (duplicates.isEmpty) str.size
+    else duplicates.map(indices => biggestIndexDifference(0 +: indices :+ str.length, indicesCache)).max
   }
+
+//  def longestSubstring(str: String): Int = {
+//    def longest(s: String): Int = {
+//      val uniqueChars = mutable.Set[Char]()
+//      var startIndex = 0
+//      var maxLength = 0
+//      s.zipWithIndex.map { case (c, i) =>
+//        if (uniqueChars.contains(c)) {
+//          val currentLength = i - startIndex
+//          if (currentLength > maxLength) maxLength = currentLength
+//          startIndex = i
+//        } else {
+//          val currentLength = (i + 1) - startIndex
+//          if (currentLength > maxLength) maxLength = currentLength
+//        }
+//        uniqueChars.add(c)
+//        println(s"maxLength:$maxLength, startIndex:$startIndex, uniqueChars:$uniqueChars")
+//      }
+//      maxLength
+//    }
+//    Math.max(longest(str), longest(str.reverse))
+//  }
 }
--- leetcode/arraysandstrings/src/test/scala/LongestSubstringSpec.scala
+++ leetcode/arraysandstrings/src/test/scala/LongestSubstringSpec.scala
@@ -16,11 +16,30 @@ class LongestSubstringSpec extends AnyFreeSpec with Matchers {
     longestSubstring("pwwkew") shouldBe 3
   }
 
+  "when the whole string is unique" in {
+    longestSubstring("abcde") shouldBe 5
+  }
+
   "when there is a 0 long unique substring" in {
     longestSubstring("") shouldBe 0
   }
 
   "when there is a 4 long unique substring at the end" in {
     longestSubstring("pwwker") shouldBe 4
+  }
+
+  "leetcode" - {
+    "when repeating char is not at the beginning of the new substring" in {
+      longestSubstring("dvdf") shouldBe 3
+    }
+    "when some special scenario in my code" in {
+      longestSubstring("ckilbkd") shouldBe 5
+    }
+    "when repeating at both ends" in {
+      longestSubstring("asjrgapa") shouldBe 6
+    }
+    "when only one white space" in {
+      longestSubstring(" ") shouldBe 1
+    }
   }
 }
